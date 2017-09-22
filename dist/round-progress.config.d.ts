804866b2e9cdc1d840ec6dcfb13f005ba742a268
Chaitanya <chaithanya.mankala@gmail.com>
2017-09-22 15:37:03 +0530

    added dist

---
+++ dist/round-progress.config.d.ts
@@ -0,0 +1,21 @@
+export interface RoundProgressDefaults {
+    radius?: number;
+    animation?: string;
+    animationDelay?: number;
+    duration?: number;
+    stroke?: number;
+    color?: string;
+    background?: string;
+    responsive?: boolean;
+    clockwise?: boolean;
+    semicircle?: boolean;
+    rounded?: boolean;
+    on?:any;
+}
+export declare class RoundProgressConfig {
+    private _options;
+    /** Configures the defaults. */
+    setDefaults(config: RoundProgressDefaults): RoundProgressDefaults;
+    /** Fetches a value from the defaults. */
+    get(key: string): any;
+}
